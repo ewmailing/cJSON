fc7fffbd15b2a244e9be5870e84b1db320dda209
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-05-13 03:30:21 -0700

    Added dll export markers

--- cJSON.h
+++ cJSON.h
@@ -28,6 +28,19 @@ extern "C"
 {
 #endif
 
+/* Windows needs to know explicitly which functions to export in a DLL. */
+#ifdef BUILD_CJSON_AS_DLL
+#	ifdef WIN32
+#		define CJSON_EXPORT __declspec(dllexport)
+#	elif defined(__GNUC__) && __GNUC__ >= 4
+#		define CJSON_EXPORT __attribute__ ((visibility("default")))
+#	else
+#		define CJSON_EXPORT
+#	endif
+#else
+#	define CJSON_EXPORT
+#endif /* BUILD_CJSON_AS_DLL */
+
 /* cJSON Types: */
 #define cJSON_False 0
 #define cJSON_True 1
@@ -59,71 +72,71 @@ typedef struct cJSON_Hooks {
 } cJSON_Hooks;
 
 /* Supply malloc, realloc and free functions to cJSON */
-extern void cJSON_InitHooks(cJSON_Hooks* hooks);
+extern CJSON_EXPORT void cJSON_InitHooks(cJSON_Hooks* hooks);
 
 
 /* Supply a block of JSON, and this returns a cJSON object you can interrogate. Call cJSON_Delete when finished. */
-extern cJSON *cJSON_Parse(const char *value);
+extern CJSON_EXPORT cJSON *cJSON_Parse(const char *value);
 /* Render a cJSON entity to text for transfer/storage. Free the char* when finished. */
-extern char  *cJSON_Print(cJSON *item);
+extern CJSON_EXPORT char  *cJSON_Print(cJSON *item);
 /* Render a cJSON entity to text for transfer/storage without any formatting. Free the char* when finished. */
-extern char  *cJSON_PrintUnformatted(cJSON *item);
+extern CJSON_EXPORT char  *cJSON_PrintUnformatted(cJSON *item);
 /* Delete a cJSON entity and all subentities. */
-extern void   cJSON_Delete(cJSON *c);
+extern CJSON_EXPORT void   cJSON_Delete(cJSON *c);
 
 /* Returns the number of items in an array (or object). */
-extern int	  cJSON_GetArraySize(cJSON *array);
+extern CJSON_EXPORT int	  cJSON_GetArraySize(cJSON *array);
 /* Retrieve item number "item" from array "array". Returns NULL if unsuccessful. */
-extern cJSON *cJSON_GetArrayItem(cJSON *array,int item);
+extern CJSON_EXPORT cJSON *cJSON_GetArrayItem(cJSON *array,int item);
 /* Get item "string" from object. Case insensitive. */
-extern cJSON *cJSON_GetObjectItem(cJSON *object,const char *string);
+extern CJSON_EXPORT cJSON *cJSON_GetObjectItem(cJSON *object,const char *string);
 
 /* For analysing failed parses. This returns a pointer to the parse error. You'll probably need to look a few chars back to make sense of it. Defined when cJSON_Parse() returns 0. 0 when cJSON_Parse() succeeds. */
-extern const char *cJSON_GetErrorPtr(void);
+extern CJSON_EXPORT const char *cJSON_GetErrorPtr(void);
 	
 /* These calls create a cJSON item of the appropriate type. */
-extern cJSON *cJSON_CreateNull(void);
-extern cJSON *cJSON_CreateTrue(void);
-extern cJSON *cJSON_CreateFalse(void);
-extern cJSON *cJSON_CreateBool(int b);
-extern cJSON *cJSON_CreateNumber(double num);
-extern cJSON *cJSON_CreateString(const char *string);
-extern cJSON *cJSON_CreateArray(void);
-extern cJSON *cJSON_CreateObject(void);
+extern CJSON_EXPORT cJSON *cJSON_CreateNull(void);
+extern CJSON_EXPORT cJSON *cJSON_CreateTrue(void);
+extern CJSON_EXPORT cJSON *cJSON_CreateFalse(void);
+extern CJSON_EXPORT cJSON *cJSON_CreateBool(int b);
+extern CJSON_EXPORT cJSON *cJSON_CreateNumber(double num);
+extern CJSON_EXPORT cJSON *cJSON_CreateString(const char *string);
+extern CJSON_EXPORT cJSON *cJSON_CreateArray(void);
+extern CJSON_EXPORT cJSON *cJSON_CreateObject(void);
 
 /* These utilities create an Array of count items. */
-extern cJSON *cJSON_CreateIntArray(const int *numbers,int count);
-extern cJSON *cJSON_CreateFloatArray(const float *numbers,int count);
-extern cJSON *cJSON_CreateDoubleArray(const double *numbers,int count);
-extern cJSON *cJSON_CreateStringArray(const char **strings,int count);
+extern CJSON_EXPORT cJSON *cJSON_CreateIntArray(const int *numbers,int count);
+extern CJSON_EXPORT cJSON *cJSON_CreateFloatArray(const float *numbers,int count);
+extern CJSON_EXPORT cJSON *cJSON_CreateDoubleArray(const double *numbers,int count);
+extern CJSON_EXPORT cJSON *cJSON_CreateStringArray(const char **strings,int count);
 
 /* Append item to the specified array/object. */
-extern void cJSON_AddItemToArray(cJSON *array, cJSON *item);
-extern void	cJSON_AddItemToObject(cJSON *object,const char *string,cJSON *item);
+extern CJSON_EXPORT void cJSON_AddItemToArray(cJSON *array, cJSON *item);
+extern CJSON_EXPORT void	cJSON_AddItemToObject(cJSON *object,const char *string,cJSON *item);
 /* Append reference to item to the specified array/object. Use this when you want to add an existing cJSON to a new cJSON, but don't want to corrupt your existing cJSON. */
-extern void cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
-extern void	cJSON_AddItemReferenceToObject(cJSON *object,const char *string,cJSON *item);
+extern CJSON_EXPORT void cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
+extern CJSON_EXPORT void	cJSON_AddItemReferenceToObject(cJSON *object,const char *string,cJSON *item);
 
 /* Remove/Detatch items from Arrays/Objects. */
-extern cJSON *cJSON_DetachItemFromArray(cJSON *array,int which);
-extern void   cJSON_DeleteItemFromArray(cJSON *array,int which);
-extern cJSON *cJSON_DetachItemFromObject(cJSON *object,const char *string);
-extern void   cJSON_DeleteItemFromObject(cJSON *object,const char *string);
+extern CJSON_EXPORT cJSON *cJSON_DetachItemFromArray(cJSON *array,int which);
+extern CJSON_EXPORT void   cJSON_DeleteItemFromArray(cJSON *array,int which);
+extern CJSON_EXPORT cJSON *cJSON_DetachItemFromObject(cJSON *object,const char *string);
+extern CJSON_EXPORT void   cJSON_DeleteItemFromObject(cJSON *object,const char *string);
 	
 /* Update array items. */
-extern void cJSON_ReplaceItemInArray(cJSON *array,int which,cJSON *newitem);
-extern void cJSON_ReplaceItemInObject(cJSON *object,const char *string,cJSON *newitem);
+extern CJSON_EXPORT void cJSON_ReplaceItemInArray(cJSON *array,int which,cJSON *newitem);
+extern CJSON_EXPORT void cJSON_ReplaceItemInObject(cJSON *object,const char *string,cJSON *newitem);
 
 /* Duplicate a cJSON item */
-extern cJSON *cJSON_Duplicate(cJSON *item,int recurse);
+extern CJSON_EXPORT cJSON *cJSON_Duplicate(cJSON *item,int recurse);
 /* Duplicate will create a new, identical cJSON item to the one you pass, in new memory that will
 need to be released. With recurse!=0, it will duplicate any children connected to the item.
 The item->next and ->prev pointers are always zero on return from Duplicate. */
 
 /* ParseWithOpts allows you to require (and check) that the JSON is null terminated, and to retrieve the pointer to the final byte parsed. */
-extern cJSON *cJSON_ParseWithOpts(const char *value,const char **return_parse_end,int require_null_terminated);
+extern CJSON_EXPORT cJSON *cJSON_ParseWithOpts(const char *value,const char **return_parse_end,int require_null_terminated);
 
-extern void cJSON_Minify(char *json);
+extern CJSON_EXPORT void cJSON_Minify(char *json);
 
 /* Macros for creating things quickly. */
 #define cJSON_AddNullToObject(object,name)		cJSON_AddItemToObject(object, name, cJSON_CreateNull())
